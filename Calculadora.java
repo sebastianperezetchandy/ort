1f4da027f8cfca3f0b96d0b5f7663f5f9dd85230
Seba <sperez@ceibal.edu.uy>
2017-11-09 20:06:33 -0300

    Se agrega resta

--- Calculadora.java
+++ Calculadora.java
@@ -7,7 +7,14 @@ public class Calculadora{
 				int op1 = Integer.parseInt(args[1]);
 				int op2 = Integer.parseInt(args[2]);
 
-				System.out.println(op1 + op2);
+				if("+".equals(args[0])){
+					System.out.println(op1 + op2);
+				}else if("-".equals(args[0])){
+					System.out.println(op1 - op2);
+				}else{
+					System.out.println(args[0] + " Operación desconocida");
+				}
+
 			} catch (Exception ex){
 				System.out.println("Parametros incorrectos");
 			}
